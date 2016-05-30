5c29240bc22dd8c84adfd980678826d9e32487ec
Rahav lussato <rahav@yallo.com>
2016-05-30 15:28:16 +0300

    fix css collisions

--- src/Select.js
+++ src/Select.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Input from 'react-input-autosize';
 import classNames from 'classnames';
-import 'flexboxgrid/dist/flexboxgrid.css';
 
 import stripDiacritics from './utils/stripDiacritics';
 
